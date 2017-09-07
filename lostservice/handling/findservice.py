7eb7ff78e99f4563ab0c537c9b9abcd35e14c04b
Darell Stoick <dstoick@geo-comm.com>
2017-09-07 11:55:13 -0500

    Respecting Projection in Request

--- lostservice/handling/findservice.py
+++ lostservice/handling/findservice.py
@@ -532,9 +532,9 @@ class FindServiceInner(object):
         :return: The service mappings for the given arcband.
         :rtype: ``list`` of ``dict``
         """
-        arcband = geom.generate_arcband(longitude, latitude, start_angle, opening_angle, inner_radius, outer_radius)
+        arcband = geom.generate_arcband(longitude, latitude, spatial_ref, start_angle, opening_angle, inner_radius, outer_radius)
         points = geom.get_vertices_for_geom(arcband)[0]
-        return self.find_service_for_polygon(service_urn, points, spatial_ref, return_shape)
+        return self.find_service_for_polygon(service_urn, points, 'urn:ogc:def:crs:EPSG::4326', return_shape)
 
     def find_service_for_polygon(self, service_urn, points, spatial_ref, return_shape=False):
         """
