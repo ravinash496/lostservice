d2cd78d707af0ca6ee6f1b69f3d07f9ffcad815f
Chad Leppanen <cleppanen@geo-comm.com>
2017-09-11 13:04:51 -0500

    Double checking node failures

--- postman_nodescript.js
+++ postman_nodescript.js
@@ -1,54 +1,17 @@
-//File paths to the various newmane files for testing.
-var testBase = './tests/postman/';
-var testFilePath = testBase + 'Tests/';
-var iterationFilePath = testBase + 'PostmanData/';
-var envFilePath = testBase + 'Environments/';
+var async = require('async'), // https://www.npmjs.com/package/async
+    newman = require('newman');
 
-
-var fs = require('fs'),         // https://nodejs.org/api/fs.html
-    newman = require('newman'),
-    path = require('path'); //https://www.npmjs.com/package/newman
-
-//This function will check in the PostmanData folder to see if the file for iterating data exists.
-//Returns a file name, or empty string.
-function checkIterationFile(file, iterationFile) {
-   var fileNameOnly = file.split('.').slice();
-    return new Promise((resolve, reject) => {
-        if(fs.existsSync(iterationFilePath + fileNameOnly[0] + '_Data.json')) {
-            iterationFile = iterationFilePath + fileNameOnly[0] + '_Data.json';
-            resolve(iterationFile);
-        } else {
-            iterationFile = '';
-            resolve(iterationFile);
-        }
-    });
-}
-
-var getFiles = function () {fs.readdir(testFilePath, (err, files) => {
-    //TODO: Assign environment file.
-    files.forEach(file => {
-        //Run our promise for the iterationData
-        checkIterationFile(file)
-        .then(function (res) {
-            newman.run({
-              collection: testFilePath + file,
-              iterationData: res,
-              reporters: 'cli',
-              timeoutRequest: 10000,
-              bail: true
-        }, function (err) {
-            if (err) { 
-                throw err;
-                console.log('i hit an error boss');
-                process.exit
-            }
-            });
-        })
-        .catch(function (error) {
-            console.log(error.message);
-        });
-    });
- });
-}
-
-getFiles();
+async.series([
+  function (next) {
+    newman.run({
+      collection: 'ECRF_Collection_Test.postman_collection.json',
+      bail: true
+    }, next);
+  },
+  function (next) {
+    newman.run({
+      collection: 'TestingPostman.postman_collection.json',
+      bail: true
+    }, next);
+  }
+], process.exit);
