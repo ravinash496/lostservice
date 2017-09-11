09fedbc09cadf24e224c817b9d557763f2b91af7
Chad Leppanen <cleppanen@geo-comm.com>
2017-09-11 12:54:52 -0500

    Edits and adds for newman tests. New recursive node script to find all files that are needed.

--- postman_nodescript.js
+++ postman_nodescript.js
@@ -1,57 +1,54 @@
-var async = require('async'), // https://www.npmjs.com/package/async
-    newman = require('newman');
+//File paths to the various newmane files for testing.
+var testBase = './tests/postman/';
+var testFilePath = testBase + 'Tests/';
+var iterationFilePath = testBase + 'PostmanData/';
+var envFilePath = testBase + 'Environments/';
 
-async.series([
-  function (next) {
-    newman.run({
-      collection: './tests/postman/Tests/ECRF_Collection_Test.postman_collection.json',
-      reporters: 'cli',
-      timeoutRequest: 10000,
-      bail: true
-    }, next);
-  },
-  function (next) {
-    newman.run({
-      collection: './tests/postman/Tests/TestingPostman.postman_collection.json',
-      reporters: 'cli',
-      timeoutRequest: 10000,
-      bail: true
-    }, next);
-  },
-  /*function (next) {
-    newman.run({
-      collection: './tests/postman/Tests/SR_Geodetic_FindService_ByValue.postman_collection.json',
-      iterationData: './tests/postman/PostmanData/SR_Geodetic_FindService_ByValue_Data.json',
-      reporters: 'cli',
-      timeoutRequest: 10000,
-      bail: true
-    }, next);
-  },*/
-  function (next) {
-    newman.run({
-      collection: './tests/postman/Tests/SR_Geodetic_ListServicesByLocation.postman_collection.json',
-      iterationData: './tests/postman/PostmanData/SR_Geodetic_ListServicesByLocation_data.json',
-      reporters: 'cli',
-      timeoutRequest: 10000,
-      bail: true
-    }, next);
-  },
-  function (next) {
-    newman.run({
-      collection: './tests/postman/Tests/SR_ListServices.postman_collection.json',
-      iterationData: './tests/postman/PostmanData/SR_ListServices_sos_data.json',
-      reporters: 'cli',
-      timeoutRequest: 10000,
-      bail: true
-    }, next);
-  },
-  function (next) {
-    newman.run({
-      collection: './tests/postman/Tests/SR_ListServices.postman_collection.json',
-      iterationData: './tests/postman/PostmanData/SR_ListServices_responder_data.json',
-      reporters: 'cli',
-      timeoutRequest: 10000,
-      bail: true
-    }, next);
-  }
-]);
+
+var fs = require('fs'),         // https://nodejs.org/api/fs.html
+    newman = require('newman'),
+    path = require('path'); //https://www.npmjs.com/package/newman
+
+//This function will check in the PostmanData folder to see if the file for iterating data exists.
+//Returns a file name, or empty string.
+function checkIterationFile(file, iterationFile) {
+   var fileNameOnly = file.split('.').slice();
+    return new Promise((resolve, reject) => {
+        if(fs.existsSync(iterationFilePath + fileNameOnly[0] + '_Data.json')) {
+            iterationFile = iterationFilePath + fileNameOnly[0] + '_Data.json';
+            resolve(iterationFile);
+        } else {
+            iterationFile = '';
+            resolve(iterationFile);
+        }
+    });
+}
+
+var getFiles = function () {fs.readdir(testFilePath, (err, files) => {
+    //TODO: Assign environment file.
+    files.forEach(file => {
+        //Run our promise for the iterationData
+        checkIterationFile(file)
+        .then(function (res) {
+            newman.run({
+              collection: testFilePath + file,
+              iterationData: res,
+              reporters: 'cli',
+              timeoutRequest: 10000,
+              bail: true
+        }, function (err) {
+            if (err) { 
+                throw err;
+                console.log('i hit an error boss');
+                process.exit
+            }
+            });
+        })
+        .catch(function (error) {
+            console.log(error.message);
+        });
+    });
+ });
+}
+
+getFiles();
