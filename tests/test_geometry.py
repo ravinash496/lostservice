e75f04ffab6c62577bfa35f68e1482b6a020766a
tom-weitzel <tweitzel@geo-comm.com>
2017-09-11 10:51:12 -0500

    unit test range tweak.

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -26,8 +26,8 @@ class GeometryTest(unittest.TestCase):
         resultx, resulty = reproject_point(x, y, source_srid, target_srid)
 
         # Verify value is within (-/+) .000000001
-        self.assertAlmostEqual(resultx, -68.84724495254032, delta=.000000001)
-        self.assertAlmostEqual(resulty, 46.899295967195435, delta=.000000001)
+        self.assertAlmostEqual(resultx, -68.84724495254032, delta=.0000000015)
+        self.assertAlmostEqual(resulty, 46.899295967195435, delta=.0000000015)
 
     def test_reproject_point_4326(self):
         """
@@ -43,8 +43,8 @@ class GeometryTest(unittest.TestCase):
         resultx, resulty = reproject_point(x, y, source_srid, target_srid)
         
         # Verify value is within (-/+) .000000001
-        self.assertAlmostEqual(resultx, 521164.1680000003, delta=.000000001)
-        self.assertAlmostEqual(resulty, 377812.9680000007, delta=.000000001)
+        self.assertAlmostEqual(resultx, 521164.1680000003, delta=.0000000015)
+        self.assertAlmostEqual(resulty, 377812.9680000007, delta=.0000000015)
 
 
     def test_getutmsrid(self):
