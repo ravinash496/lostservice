27bdef1347391b77076168f682e1b416b0c9aa6a
pmoprhy <michael.sugarray.anderson@gmail.com>
2017-11-27 14:15:25 -0600

    S-02611 Default Routes CivicMatchingRules Mode

--- lostservice/defaultroutes/defaultroutehandler.py
+++ lostservice/defaultroutes/defaultroutehandler.py
@@ -17,12 +17,22 @@ import abc
 from abc import ABC
 from enum import Enum
 from lostservice.db.gisdb import GisDbInterface
+from lostservice.model.requests import FindServiceRequest
+from typing import List
+from lostservice.model.civic import CivicAddress
+
+invalid_field_name = "invalid field name"
 
 
 class DefaultRouteModeEnum(Enum):
     OverrideRoute = "OverrideRoute"
     ExistingRoute = "ExistingRoute"
-    CivicMatchingRule = "CivicMatchingRule"
+    CivicMatchingRules = "CivicMatchingRules"
+
+
+class CivicMatchingModeEnum(Enum):
+    OverrideRoute = "OverrideRoute"
+    ExistingRoute = "ExistingRoute"
 
 
 class DefaultSetting(ABC):
@@ -39,7 +49,7 @@ class DefaultSetting(ABC):
         self.urn = urn
 
     @abc.abstractmethod
-    def get_uri(self) -> str:
+    def get_uri(self, request: FindServiceRequest) -> str:
         """
         Get the uri
         :return: the uri
@@ -60,7 +70,7 @@ class OverrideRouteSetting(DefaultSetting):
         super().__init__(mode, urn)
         self.uri = uri
 
-    def get_uri(self):
+    def get_uri(self, request: FindServiceRequest):
         return self.uri
 
 
@@ -80,7 +90,7 @@ class ExistingRouteSetting(DefaultSetting):
         self.boundary_id = boundary_id
         self._db_wrapper = db_wrapper
 
-    def get_uri(self):
+    def get_uri(self, request: FindServiceRequest):
         """
         Looks up  the uri in the database table using the boundaryid passed into the constructor
         :return: Mone or the matching rows in the table
@@ -92,6 +102,173 @@ class ExistingRouteSetting(DefaultSetting):
             return None
         else:
             return matching_boundary[0]['routeuri']
+
+
+class Condition(object):
+    """
+    class to wrap civic mapping rule condition
+    """
+    def __init__(self, cond: dict):
+        self.field = cond['field']
+        self.value = cond['value']
+
+
+class CivicMatchingRule(ABC):
+    """
+    some
+    """
+    def __init__(self, name: str, conditions: List[Condition], mode: CivicMatchingModeEnum):
+        self.name: str = name
+        self.conditions: [Condition] = conditions
+        self.mode: CivicMatchingModeEnum = mode
+
+
+class CivicOverrideMatchingRule(CivicMatchingRule):
+    """
+    matching rule that uses OverrideRoute mode
+    """
+    def __init__(self, name: str, conditions: List[Condition], mode: CivicMatchingModeEnum, uri: str):
+        super().__init__(name, conditions, mode)
+        self.uri = uri
+
+
+class CivicExistingMatchingRule(CivicMatchingRule):
+    """
+    matching rule that uses ExistingRoute mode
+    """
+
+    def __init__(self, name: str, conditions: List[Condition], mode: CivicMatchingModeEnum, boundaryid: str):
+        super().__init__(name, conditions, mode)
+        self.boundaryid = boundaryid
+
+
+class CivicMatchingSetting(DefaultSetting):
+    """
+    A class to wrap a CivicMatchingRules default setting from the config
+    """
+
+    def __init__(self, mode: str, urn: str, rules: List[dict],  db_wrapper: GisDbInterface):
+        """
+
+        :param mode: The mode for the default setting, should be a member of  DefaultRouteModeEnum
+        :param urn:  The service urn this policy applies to
+        :param rules: The civic matching rules
+        :param db_wrapper: the database wrapper to do the queries
+        """
+        super().__init__(mode, urn)
+        self.rules: [CivicMatchingRule] = self.build_rules(rules)
+        self._db_wrapper = db_wrapper
+
+    def get_uri(self, request: FindServiceRequest) -> str or None:
+        """
+        find an matching rule and return the uri
+        :return:
+        """
+        # find matching rule
+        for rule in self.rules:
+                if isinstance(request.location.location, CivicAddress):
+                    civic_address = request.location.location
+                    if self.civic_location_matches_rule_conditions(civic_address, rule.conditions):
+                        if isinstance(rule, CivicOverrideMatchingRule):
+                            return rule.uri
+                        elif isinstance(rule, CivicExistingMatchingRule):
+                            matching_boundary = self._db_wrapper.get_boundaries_for_previous_id(
+                                rule.boundaryid,
+                                self._db_wrapper.get_urn_table_mappings()[self.urn])
+                            if not matching_boundary:
+                                return None
+                            else:
+                                return matching_boundary[0]['routeuri']
+
+        return None
+
+    def civic_location_matches_rule_conditions(self, civic_address: CivicAddress, conditions: List[Condition]) -> bool:
+        """
+        Return true if civic address matches all the conditions passed in
+        :param civic_address:
+        :param conditions:
+        :return:
+        """
+        for condition in conditions:
+            civic_value = self.get_condition_value(civic_address, condition.field)
+            if civic_address != invalid_field_name:
+                if civic_value.lower() != condition.value.lower():
+                    return False
+            else:
+                return False
+        return True
+
+    @staticmethod
+    def get_condition_value(civic_address: CivicAddress, field: str) -> str:
+        """
+        Get the value in the civic address field, or return "invalid string"
+        :param civic_address:
+        :param field:
+        :return:
+        """
+        field = field.lower()
+        switcher = {
+            "country": civic_address.country,
+            "a1": civic_address.a1,
+            "a2": civic_address.a2,
+            "a3": civic_address.a3,
+            "a4": civic_address.a4,
+            "a5": civic_address.a5,
+            "a6": civic_address.a6,
+            "prm": civic_address.prm,
+            "prd": civic_address.prd,
+            "rd": civic_address.rd,
+            "sts": civic_address.sts,
+            "pod": civic_address.pod,
+            "pom": civic_address.pom,
+            "rdsec": civic_address.rdsec,
+            "rdbr": civic_address.rdbr,
+            "rdsubr": civic_address.rdsubr,
+            "hno": civic_address.hno,
+            "hns": civic_address.hns,
+            "lmk": civic_address.lmk,
+            "loc": civic_address.loc,
+            "flr": civic_address.flr,
+            "nam": civic_address.nam,
+            "pc": civic_address.pc,
+            "bld": civic_address.bld,
+            "unit": civic_address.unit,
+            "room": civic_address.room,
+            "seat": civic_address.seat,
+            "plc": civic_address.plc,
+            "pcn": civic_address.pcn,
+            "pobox": civic_address.pobox,
+            "addcode": civic_address.addcode,
+            "stp": civic_address.stp,
+            "stps": civic_address.stps
+        }
+        return switcher.get(field, invalid_field_name)
+
+    @staticmethod
+    def build_rules(rules: List[dict]) -> List[CivicMatchingRule]:
+        """
+        given some json build a list of rules
+        :param rules: the json that represents the list of rules
+        :return:
+        """
+        civic_matching_rules: [CivicMatchingRule] = []
+
+        for rule in rules:
+            conditions: [Condition] = []
+            for condition in rule['conditions']:
+                conditions.append(Condition(condition))
+            if 'uri' in rule:
+                civic_matching_rules.append(CivicOverrideMatchingRule(
+                    rule['name'],
+                    conditions,
+                    CivicMatchingModeEnum.OverrideRoute,
+                    rule['uri']))
+            else:
+                civic_matching_rules.append(CivicExistingMatchingRule(rule['name'],
+                                                                      conditions,
+                                                                      CivicMatchingModeEnum.OverrideRoute,
+                                                                      rule['boundaryid']))
+        return civic_matching_rules
 
 
 class DefaultRouteConfigWrapper(object):
@@ -125,49 +302,46 @@ class DefaultRouteConfigWrapper(object):
         # settings should be a dictionary with the first key == 'default_routes'
         # the value of settings['default_routes] should be a list of dictionaries
         # where each dictionary has three keys; 'OverrideRoute', 'urn' and 'uri'
-        base_msg = "Error in lostservice.ini file. The default_routing_civic_policy setting is mis-configured"
         if 'default_routes' in settings:
             # first key is 'default_routes' , so far so good
             list_settings = settings['default_routes']
             if isinstance(list_settings, list):
                 # the value of the first key is a list
                 for setting in list_settings:
-                    # check that this is a dictionary with three keys
+                    # check that this is a dictionary with the right number of keys
                     if isinstance(setting, dict):
                         # it's a dictionary, now check that it has the right keys
                         if 'mode' not in setting:
                             # doesn't have the 'mode' key
-                            err_msg = 'You must specify the mode for each item.'
-                            raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
+                            self._error('You must specify the mode for each item.')
                         else:
                             # it has the 'mode'
                             # check it it's set to a valid value
                             if setting['mode'] not in DefaultRouteModeEnum.__members__:
-                                err_msg = 'Unsupported mode: {0}'.format(setting['mode'])
-                                raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
+                                self._error('Unsupported mode: {0}'.format(setting['mode']))
                         if 'urn' not in setting:
-                            err_msg = 'You must specify the urn for each item.'
-                            raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
-                        if setting['mode'] == 'OverrideRoute':
+                            self._error('You must specify the urn for each item.')
+                        if setting['mode'] == DefaultRouteModeEnum.OverrideRoute.value:
                             if 'uri' not in setting:
-                                err_msg = 'You must specify the uri for each item that is in ' \
-                                    'OverrideRoute mode.'
-                                raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
-                        elif setting['mode'] == 'ExistingRoute':
+                                self._error('You must specify the uri for each item that is in '
+                                            'OverrideRoute mode.')
+                        elif setting['mode'] == DefaultRouteModeEnum.ExistingRoute.value:
                             if 'boundaryid' not in setting:
-                                err_msg = 'You must specify the boundaryid for each item that is in ' \
-                                    'ExistingRoute mode.'
-                                raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
+                                self._error('You must specify the boundaryid for each item that is in '
+                                            'ExistingRoute mode.')
+                        elif setting['mode'] == DefaultRouteModeEnum.CivicMatchingRules.value:
+                            if 'rules' not in setting:
+                                self._error('You must specify the rules for each item that is in '
+                                            'CivicMatchingRules mode.')
+                            elif self._check_rules(setting['rules']):
+                                pass
                     else:
                         # the value of each the setting is not a dictionary, that's bad
-                        err_msg = 'Each entry in the default_routes array must be an object.'
-                        raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
+                        self._error('Each entry in the default_routes array must be an object.')
             else:
-                err_msg = 'The first value must be an array of default route objects.'
-                raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
+                self._error('The first value must be an array of default route objects.')
         else:
-            err_msg = 'The first object name for the default_routing_civic_policy must be default_routes.'
-            raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
+            self._error('The first object name for the default_routing_civic_policy must be default_routes.')
 
         if settings is None:
             return None
@@ -176,13 +350,62 @@ class DefaultRouteConfigWrapper(object):
             default_settings: [DefaultSetting] = []
             for setting in default_routes:
                 if setting['mode'] == DefaultRouteModeEnum.OverrideRoute.value:
-                    default_settings.append(OverrideRouteSetting(setting['mode'], setting['urn'],setting['uri']))
+                    default_settings.append(OverrideRouteSetting(setting['mode'], setting['urn'], setting['uri']))
                 elif setting['mode'] == DefaultRouteModeEnum.ExistingRoute.value:
                     default_settings.append(ExistingRouteSetting(setting['mode'],
                                                                  setting['urn'],
                                                                  setting['boundaryid'],
                                                                  self._db))
+                elif setting['mode'] == DefaultRouteModeEnum.CivicMatchingRules.value:
+                    default_settings.append(CivicMatchingSetting(setting['mode'],
+                                                                 setting['urn'],
+                                                                 setting['rules'],
+                                                                 self._db))
             return default_settings
+
+    def _check_rules(self, rules) -> bool:
+        """
+        Basic checks for the rules element , returns true if valid, raises a configuration exception if not
+        :param
+        :param rules: a list of rules for a default routing civic policy
+        :return: true if it passes basic checks for validity
+        """
+        if isinstance(rules, list):
+            # rules are there check them
+            # check that the value is a dict
+            for rule in rules:
+                if isinstance(rule, dict):
+                    if 'name' not in rule:
+                        self._error('Each rule must have a name element.')
+                    if 'conditions' not in rule:
+                        self._error('Each rule must have a conditions element.')
+                    if 'mode' not in rule:
+                        self._error('Each rule must have a mode element.')
+                    else:
+                        if rule['mode'] not in CivicMatchingModeEnum.__members__:
+                            self._error('Each rule\'s mode must be either OverrideRoute or ExistingRoute')
+                        else:
+                            if rule['mode'] == CivicMatchingModeEnum.OverrideRoute.value and 'uri' not in rule:
+                                self._error('Each rule where mode is OverrideRoute must have a uri element.')
+                            if rule['mode'] == CivicMatchingModeEnum.ExistingRoute.value and 'boundaryid' not in rule:
+                                self._error('Each rule where mode is ExistingRoute must have a boundaryid element.')
+                else:
+                    self._error('Each rule in rules must be a dictionary of key value pairs.')
+        else:
+            self._error('The rules element value must be an array of rules.')
+
+        # if it makes it here it passed all the tests.
+        return True
+
+    @staticmethod
+    def _error(err_msg):
+        """
+        Raise a ConfigurationException
+        :param err_msg: the rest of the error message
+        :return: None
+        """
+        base_msg = "Error in lostservice.ini file. The default_routing_civic_policy setting is mis-configured"
+        raise ConfigurationException('{0} : {1}'.format(base_msg, err_msg))
 
 
 class DefaultRouteHandler(object):
@@ -202,9 +425,9 @@ class DefaultRouteHandler(object):
     def check_default_route(self, request):
         # Check for default routes
         # if there are none then throw a NotFoundException (return a notFound LoST error)
-        default_route_uri = self._get_default_civic_route(request.service)
+        default_route_uri = self._get_default_civic_route(request)
         if default_route_uri is None:
-            raise NotFoundException('The server could not find an answer to the query.', None)
+            raise NotFoundException('The server could not find an answer to the query.')
         else:
             # Create a default mapping given just a uri
             new_dict = {'serviceurn': request.service,
@@ -220,10 +443,10 @@ class DefaultRouteHandler(object):
 
             return default_mapping
 
-    def _get_default_civic_route(self, service_urn: str) -> str or None:
+    def _get_default_civic_route(self, request: FindServiceRequest) -> str or None:
         """
         Returns a uri if there is a match in the config file for the passed in urn
-        :param service_urn:
+        :param request: The request object
         :return: uri or None
         """
         # get default route policies
@@ -233,8 +456,8 @@ class DefaultRouteHandler(object):
 
         # get any matching configured urn's in the config, should only be 1 or 0
         matches: [DefaultSetting] = \
-            [default_route for default_route in default_routes if default_route.urn == service_urn]
+            [default_route for default_route in default_routes if default_route.urn == request.service]
         if not matches:
             return None
         else:
-            return matches[0].get_uri()
+            return matches[0].get_uri(request)
--- tests/test_defaultroutes_defaultroutehandler_defaultroutehandler.py
+++ tests/test_defaultroutes_defaultroutehandler_defaultroutehandler.py
@@ -4,8 +4,12 @@
 import unittest
 from unittest.mock import patch
 from unittest.mock import MagicMock
-from lostservice.defaultroutes.defaultroutehandler import DefaultSetting, DefaultRouteHandler, OverrideRouteSetting
+from lostservice.defaultroutes.defaultroutehandler import DefaultSetting, \
+    DefaultRouteHandler, OverrideRouteSetting, CivicMatchingSetting, DefaultRouteModeEnum
 from lostservice.defaultroutes.defaultroutehandler import ExistingRouteSetting
+from lostservice.model.requests import FindServiceRequest
+from lostservice.model.location import Location
+from lostservice.model.civic import CivicAddress
 
 
 class DefaultRouteHandlerTest(unittest.TestCase):
@@ -23,7 +27,8 @@ class DefaultRouteHandlerTest(unittest.TestCase):
         mock_config.settings_for_default_route.return_value = default_routes
 
         target = DefaultRouteHandler(mock_config)
-        actual = target._get_default_civic_route('urn:nena:service:sos')
+        request = FindServiceRequest(service='urn:nena:service:sos')
+        actual = target._get_default_civic_route(request)
 
         self.assertEqual(actual, 'sip:sos@oakgrove.ngesi.maine.gov')
 
@@ -39,7 +44,8 @@ class DefaultRouteHandlerTest(unittest.TestCase):
         mock_config.settings_for_default_route.return_value = default_routes
 
         target = DefaultRouteHandler(mock_config)
-        actual = target._get_default_civic_route('urn:nena:service:sos.ambulance')
+        request = FindServiceRequest(service='urn:nena:service:sos.ambulance')
+        actual = target._get_default_civic_route(request)
 
         self.assertEqual(actual, None)
 
@@ -62,6 +68,111 @@ class DefaultRouteHandlerTest(unittest.TestCase):
         mock_db.get_boundaries_for_previous_id.return_value = [{'routeuri': 'sip:sos@portlandpd.ngesi.maine.gov'}]
 
         target = DefaultRouteHandler(mock_config)
-        actual = target._get_default_civic_route('urn:nena:service:sos.police')
+        request = FindServiceRequest(service='urn:nena:service:sos.police')
+        actual = target._get_default_civic_route(request)
+
+        self.assertEqual(actual, 'sip:sos@portlandpd.ngesi.maine.gov')
+
+    @patch('lostservice.defaultroutes.defaultroutehandler.DefaultRouteConfigWrapper')
+    @patch('lostservice.db.gisdb.GisDbInterface')
+    def test__get_default_civic_route_civic_matching_override(self, mock_config, mock_db):
+        mock_config.settings_for_default_route = MagicMock()
+        rules = [
+            {
+                "name": "some name",
+                "conditions": [
+                    {
+                        "field": "A2",
+                        "value": "Waldo"
+                    }
+                ],
+                "mode": "OverrideRoute",
+                "uri": "SIP:+2075555583@strongFD.ngesi.maine.gov"
+            },
+            {
+                "name": "another rule",
+                "conditions": [
+                    {
+                        "field": "A2",
+                        "value": "Piscataquis"
+                    }
+                ],
+                "mode": "ExistingRoute",
+                "boundaryid": "{83B81936-29F7-441C-B3F1-5ECE9FA80E50}"
+            }
+        ]
+
+        default_routes: [DefaultSetting] = [OverrideRouteSetting(DefaultRouteModeEnum.OverrideRoute.value,
+                                                                 'urn:nena:service:sos',
+                                                                 'sip:sos@oakgrove.ngesi.maine.gov'),
+                                            ExistingRouteSetting(DefaultRouteModeEnum.ExistingRoute.value,
+                                                                 'urn:nena:service:sos.police',
+                                                                 '{AFF10CC6-54F2-4A43-AE12-D8881CD550A4}',
+                                                                 mock_db),
+                                            CivicMatchingSetting(DefaultRouteModeEnum.CivicMatchingRules.value,
+                                                                 'urn:nena:service:sos.fire',
+                                                                 rules,
+                                                                 mock_db)]
+
+        mock_config.settings_for_default_route.return_value = default_routes
+
+        target = DefaultRouteHandler(mock_config)
+        request = FindServiceRequest(location=Location('myID', 'civic', CivicAddress(a2='Waldo')),
+                                     service='urn:nena:service:sos.fire')
+        actual = target._get_default_civic_route(request)
+
+        self.assertEqual(actual, 'SIP:+2075555583@strongFD.ngesi.maine.gov')
+
+    @patch('lostservice.defaultroutes.defaultroutehandler.DefaultRouteConfigWrapper')
+    @patch('lostservice.db.gisdb.GisDbInterface')
+    def test__get_default_civic_route_civic_matching_existing(self, mock_config, mock_db):
+        mock_config.settings_for_default_route = MagicMock()
+        rules = [
+            {
+                "name": "some name",
+                "conditions": [
+                    {
+                        "field": "A2",
+                        "value": "Waldo"
+                    }
+                ],
+                "mode": "OverrideRoute",
+                "uri": "SIP:+2075555583@strongFD.ngesi.maine.gov"
+            },
+            {
+                "name": "another rule",
+                "conditions": [
+                    {
+                        "field": "A2",
+                        "value": "Piscataquis"
+                    }
+                ],
+                "mode": "ExistingRoute",
+                "boundaryid": "{83B81936-29F7-441C-B3F1-5ECE9FA80E50}"
+            }
+        ]
+
+        default_routes: [DefaultSetting] = [OverrideRouteSetting(DefaultRouteModeEnum.OverrideRoute.value,
+                                                                 'urn:nena:service:sos',
+                                                                 'sip:sos@oakgrove.ngesi.maine.gov'),
+                                            ExistingRouteSetting(DefaultRouteModeEnum.ExistingRoute.value,
+                                                                 'urn:nena:service:sos.police',
+                                                                 '{AFF10CC6-54F2-4A43-AE12-D8881CD550A4}',
+                                                                 mock_db),
+                                            CivicMatchingSetting(DefaultRouteModeEnum.CivicMatchingRules.value,
+                                                                 'urn:nena:service:sos.fire',
+                                                                 rules,
+                                                                 mock_db)]
+
+        mock_config.settings_for_default_route.return_value = default_routes
+        mock_db.get_urn_table_mappings = MagicMock()
+        mock_db.get_urn_table_mappings.return_value = {'urn:nena:service:sos.fire': 'esbfire'}
+        mock_db.get_boundaries_for_previous_id = MagicMock()
+        mock_db.get_boundaries_for_previous_id.return_value = [{'routeuri': 'sip:sos@portlandpd.ngesi.maine.gov'}]
+
+        target = DefaultRouteHandler(mock_config)
+        request = FindServiceRequest(location=Location('myID', 'civic', CivicAddress(a2='Piscataquis')),
+                                     service='urn:nena:service:sos.fire')
+        actual = target._get_default_civic_route(request)
 
         self.assertEqual(actual, 'sip:sos@portlandpd.ngesi.maine.gov')
